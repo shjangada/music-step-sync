9d7cd32579213e864e77284613e081eaae94117a
ethanbg2 <ethanbg2@illinois.edu>
2023-09-29 01:19:09 -0500

    add boiler plate

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Spotify } from "./src/features/SpotifyIntegration";
+import { Steps } from "./src/features/StepDetection";
+import { StyleSheet, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello everyone</Text>
-      <StatusBar style="auto" />
+      <Spotify></Spotify>
+      <Steps></Steps>
     </View>
   );
 }
